544a8eb55dccd3728cc1d151ad6f51c047c95760
shoeone96 <85065626+shoeone96@users.noreply.github.com>
2021-06-08 20:11:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,12 @@ from pymongo import MongoClient
 import jwt
 import datetime
 import hashlib
-from flask import Flask, render_template, jsonify, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
-from crawling import get_movie_info, get_movie_summary
+from crawling import get_movie_info
 import os
 from math import ceil
-
-app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
-
-SECRET_KEY = 'SPARTA'
 
 
 app = Flask(__name__)
@@ -25,7 +19,7 @@ SECRET_KEY = 'SPARTA'
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-movie_list = get_movie_info()   
+movie_list = get_movie_info()
 page_count = ceil(len(movie_list) / 20)
 
 @app.route('/')
@@ -40,20 +34,23 @@ def favicon():
 def login():
     return render_template('log-in.html')
 
-@app.route('/login', methods=['POST'])
-def sign_up():
-    # 회원가입
-    username_receive = request.form['username_give']
-    password_receive = request.form['password_give']
-    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    # DB에 저장
-    return jsonify({'result': 'success'})
-
 @app.route('/login/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
+
+@app.route('/login/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+    }
+    db.userscinema.insert_one(doc)
+    return jsonify({'result': 'success'})
 
 @app.route('/page', methods=['GET'])
 def page():
@@ -61,12 +58,26 @@ def page():
 
     return render_template('index.html', movie_list=movie_list[20 * order:20 * (order + 1)], page_count=page_count)
 
-@app.route('/detail', methods=['GET'])
-def detail():
-    code = int(request.args.get('code'))
-    detail_info = get_movie_summary(code)
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
 
-    return render_template('detail.html', detail_info=detail_info)
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
