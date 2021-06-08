316d0ca0f7dea4a69e515818faf9dab66f245752
shoeone96 <85065626+shoeone96@users.noreply.github.com>
2021-06-08 14:18:19 +0900

    Update app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('log-in.html')
+
+@app.route('/login', methods=['POST'])
+def sign_up():
+    # 회원가입
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    # DB에 저장
+    return jsonify({'result': 'success'})
+
+@app.route('/login/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
