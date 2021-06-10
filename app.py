ac931d278fa49a2d87cb28000e37b9eca5dac37c
shoeone96 <ijung-won@ijung-won-ui-MacBookPro.local>
2021-06-10 11:33:05 +0900

    save

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from crawling import movies, get_movie_summary
 import os
 from math import ceil
 
-
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
@@ -20,28 +19,33 @@ SECRET_KEY = 'SPARTA'
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
+
 @app.route('/')
 def home():
     page_count = ceil(len(movies) / 20)
-    
+
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        username = db.users.find_one({"id":payload['id']})
-        return render_template('index.html',  movie_list=movies[:20], page_count=page_count, username=username['id'], user_info=user_info)
+        username = db.users.find_one({"id": payload['id']})
+        return render_template('index.html', movie_list=movies[:20], page_count=page_count, username=username)
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        return render_template('index.html', movie_list=movies[:20], page_count=page_count)
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return render_template('index.html', movie_list=movies[:20], page_count=page_count)
+
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
+
 
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('log-in.html', msg=msg)
+
 
 @app.route('/login/check_dup', methods=['POST'])
 def check_dup():
@@ -49,28 +53,30 @@ def check_dup():
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
+
 @app.route('/login/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
     }
     db.userscinema.insert_one(doc)
     return jsonify({'result': 'success'})
+
 
 @app.route('/page', methods=['GET'])
 def page():
     page_count = ceil(len(movies) / 20)
     order = int(request.args.get('order'))
-    
+
     return render_template('index.html', movie_list=movies[20 * order:20 * (order + 1)], page_count=page_count)
+
 
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
-
     # 로그인
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
@@ -80,14 +86,15 @@ def sign_in():
 
     if result is not None:
         payload = {
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
 
 @app.route('/detail', methods=['GET'])
 def detail():
@@ -96,27 +103,37 @@ def detail():
 
     return render_template('detail.html', detail_info=detail_info)
 
+
 @app.route('/review', methods=['GET'])
 def show_review():
     code = int(request.args.get('id'))
-    review_list = list(db.usersreview.find({'code':code}, {'_id':False}))
+    review_list = list(db.usersreview.find({'code': code}, {'_id': False}))
 
-    return jsonify({'review_list': review_list })
+    return jsonify({'review_list': review_list})
+
 
 @app.route('/review/add', methods=['POST'])
 def get_review():
-    receive = request.get_json()
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.userscinema.find_one({"id": payload['id']})
+        receive = request.get_json()
 
-    doc = {
-        "code": receive['code'],
-        "grade": receive['grade'],
-        "comment": receive['comment'],
-        "date": receive['date']
-    }
+        doc = {
+            'user_id': user_info['id'],
+            "code": receive['code'],
+            "grade": receive['grade'],
+            "comment": receive['comment'],
+            "date": receive['date']
+        }
 
-    db.usersreview.insert_one(doc)
+        db.usersreview.insert_one(doc)
 
-    return jsonify({'result': 'success'})
+        return jsonify({'result': 'success'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
